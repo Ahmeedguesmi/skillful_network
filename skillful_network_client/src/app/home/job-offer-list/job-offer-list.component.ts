daee32471d45afbf797b89101d5310f9add2c3d4
Anaëlle <anaelle.alenda@gmail.com>
2020-04-06 01:02:05 +0200

    AA_3.R.1.1A JOB-OFFER modified to insert pagination - add pageSize select

--- skillful_network_client/src/app/home/job-offer-list/job-offer-list.component.ts
+++ skillful_network_client/src/app/home/job-offer-list/job-offer-list.component.ts
@@ -1,11 +1,16 @@
 import { MOCK_CANDIDATURE } from './../../shared/models/mock.candidature';
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatTableDataSource} from '@angular/material/table';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { CandidatureService } from 'src/app/shared/services/candidature.service';
 import { IPost } from 'src/app/shared/models/mock.candidature';
-import {JobOfferService} from '../../shared/services/job-offer.service';
+import { JobOfferService } from '../../shared/services/job-offer.service';
+import { JobOffer } from '../../shared/models/job-offer';
+import { Candidature } from '../../shared/models/candidature';
+import { Observable } from 'rxjs';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-job-offer-list',
@@ -13,21 +18,69 @@ import {JobOfferService} from '../../shared/services/job-offer.service';
   styleUrls: ['./job-offer-list.component.scss']
 })
 export class JobOfferListComponent implements OnInit {
+
+  jobOfferId: number;
   displayedColumns: string[] = ['name', 'company', 'type', 'dateBeg', 'dateUpload', 'statut', 'plus_info'];
-// Affichage des données à l'aide du  service depuis un MOCK, cela pour test sans Backend
-  dataSource = new MatTableDataSource(this.data.findAllMock());
-// Affichage des données à l'aide du  service depuis le Backend
-//  dataSource = new MatTableDataSource(this.data.findAll());
-  public listCandidature: IPost[] = MOCK_CANDIDATURE;
+  dataSource = null;
 
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
-  constructor(public cs: CandidatureService, public data: JobOfferService) { }
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-  ngOnInit(): void {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+  //initial page size
+  pageEvent: PageEvent;
+  pageSize: number;
+  pageSizeOptions: number[] = [10, 25, 50, 100];
+  pageIndex: number;
+  
+  hidePageSize: boolean = false;
+  showFirstLastButtons: boolean = false;
+  
+  //Pagination variables
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  //Event page to get size entities and page index
+  // pageEvent($event) {
+  //   this.pageSize = $event.pageSize;
+  //   this.pageIndex = $event.pageIndex;
+  // }
+
+  constructor(public candidatureService: CandidatureService, public offerService: JobOfferService) {
   }
 
+  ngOnInit(): void {
+    this.getJobOffers(this.paginator);
+  }
+
+  getJobOffers(event?:PageEvent) {
+    if (event == null) {
+      this.pageSize =10;
+      this.pageIndex =1;
+    } else {
+      this.pageSize= event.pageSize;
+      this.pageIndex = event.pageIndex;
+    }
+      this.offerService.findAll(this.pageIndex,this.pageSize).then(res => {
+      this.dataSource = new MatTableDataSource<JobOffer>(res);
+      console.log("pageSize function: " + this.pageSize);
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+  searchByNameOrCompany(form: NgForm) {
+    this.offerService.getOffersBySearch(form.value.keyword, form.value.page, form.value.size).then(res => {
+      this.dataSource = new MatTableDataSource<JobOffer>(res.content);
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+  //this.dataSource.paginator = this.paginator;
+  //this.dataSource.sort = this.sort;
+
+  findApplicationJob(jobOfferId: number) {
+    this.candidatureService.findApplicationJobByJobOffer(jobOfferId).then(res => {
+      this.dataSource = new MatTableDataSource<Candidature>(res);
+    });
+  }
 }
+
+
